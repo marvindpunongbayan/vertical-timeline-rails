4f280ae6a144eb69192ef4a131486780cb38ae24
Marvin Punongbayan <marvindpunongbayan@gmail.com>
2020-04-28 18:03:23 +0800

    Remove element CSS Styles to avoid affecting other apps

--- lib/vertical-timeline-rails/version.rb
+++ lib/vertical-timeline-rails/version.rb
@@ -1,8 +1,8 @@
 module Vertical
   module Timeline
     module Rails
-      VERSION = '0.1.0'
-      VERTICAL_TIMELINE_VERSION = '0.1.0'
+      VERSION = '0.1.1'
+      VERTICAL_TIMELINE_VERSION = '0.1.1'
     end
   end
 end
